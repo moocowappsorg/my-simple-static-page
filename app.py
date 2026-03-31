854bf4beb61afed7f0c5f4e51395999bb38cb45c
michaelberinski <michaelberinski@gmail.com>
2026-03-31 01:10:51 +0100

    Reorganize to standard Flask structure

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:path>')
-def static_files(path):
-    return send_from_directory('.', path)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
